879504f1a9f092b92f60df8d7364d7ff345194e0
Abhishek Puri <puri.abhishek14@gmail.com>
2018-01-22 01:50:20 +0530

    Using DrgProviderDetailsQuery in /providers endpoint

--- app/controllers/drg_provider_details_controller.rb
+++ app/controllers/drg_provider_details_controller.rb
@@ -1,8 +1,16 @@
 class DrgProviderDetailsController < ApplicationController
 
   def index
-    @drg_provider_details = DrgProviderDetail.includes(health_care_provider: [{city: :state}, :hospital_referral_region])
-                                .paginate(page: params[:page], per_page: params[:page_size])
+    query = DrgProviderDetailsQuery.new(search_params.to_h)
+    if query.valid?
+      query_result = query.execute
+      @drg_provider_details = query_result.records
+      response.set_header('X-Total-Record-Count', query_result.total_record_count)
+      response.set_header('X-Page-Number', query_result.page)
+      response.set_header('X-Page-Size', query_result.page_size)
+    else
+      render status: :bad_request
+    end
   end
 
   private
@@ -11,6 +19,7 @@ class DrgProviderDetailsController < ApplicationController
     params.permit(:page, :page_size, :state,
                   :max_discharges, :min_discharges,
                   :max_average_covered_charges, :min_average_covered_charges,
-                  :max_average_medicare_payments, :min_average_medicare_payments)
+                  :max_average_medicare_payments, :min_average_medicare_payments,
+                  :max_average_total_payments, :min_average_total_payments)
   end
 end
--- spec/controllers/drg_provider_details_controller_spec.rb
+++ spec/controllers/drg_provider_details_controller_spec.rb
@@ -4,55 +4,67 @@ RSpec.describe DrgProviderDetailsController, type: :controller do
   render_views
 
   context '#index' do
-    it 'should render the json array for' do
-      drg_provider_details = create_list(:drg_provider_detail, 5)
+    it 'should use DrgProviderDetailsQuery to fetch records' do
+      stubbed_query = OpenStruct.new(:valid? => true, execute: QueryResult.new([], 0, 1, 30))
+      expect(DrgProviderDetailsQuery).to receive(:new).with({}).and_return(stubbed_query)
 
       get :index, format: :json
 
-      providers = JSON.parse(response.body, symbolize_names: true)
-      expect(response).to render_template('drg_provider_details/index')
-      expect(assigns(:drg_provider_details).to_a).to eq(drg_provider_details)
-      expect(providers.map {|provider| provider[:providerName]}).to eq(drg_provider_details.collect(&:health_care_provider).collect(&:name))
+      expect(response.status).to eq(200)
     end
 
-    it 'should render all health care provider fields by default' do
-      health_care_provider = create(:drg_provider_detail).health_care_provider
+    context 'valid request' do
+      it 'should render the json array for' do
+        drg_provider_details = create_list(:drg_provider_detail, 5)
 
-      get :index, format: :json
+        get :index, format: :json
 
-      provider = JSON.parse(response.body, symbolize_names: true).first
-      expect(provider[:providerName]).to eq(health_care_provider.name)
-      expect(provider[:providerStreetAddress]).to eq(health_care_provider.street)
-      expect(provider[:providerCity]).to eq(health_care_provider.city.name)
-      expect(provider[:providerState]).to eq(health_care_provider.city.state.abbreviation)
-      expect(provider[:providerZipCode]).to eq(health_care_provider.zip_code)
-      expect(provider[:hospitalReferralRegionDescription]).to eq(health_care_provider.hospital_referral_region.description)
+        providers = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to render_template('drg_provider_details/index')
+        expect(assigns(:drg_provider_details).to_a).to eq(drg_provider_details)
+        expect(providers.map {|provider| provider[:providerName]}).to eq(drg_provider_details.collect(&:health_care_provider).collect(&:name))
+      end
+
+      it 'should render all health care provider fields by default' do
+        health_care_provider = create(:drg_provider_detail).health_care_provider
+
+        get :index, format: :json
+
+        provider = JSON.parse(response.body, symbolize_names: true).first
+        expect(provider[:providerName]).to eq(health_care_provider.name)
+        expect(provider[:providerStreetAddress]).to eq(health_care_provider.street)
+        expect(provider[:providerCity]).to eq(health_care_provider.city.name)
+        expect(provider[:providerState]).to eq(health_care_provider.city.state.abbreviation)
+        expect(provider[:providerZipCode]).to eq(health_care_provider.zip_code)
+        expect(provider[:hospitalReferralRegionDescription]).to eq(health_care_provider.hospital_referral_region.description)
+      end
+
+      it 'should render all payments fields by default' do
+        drg_provider_detail = create(:drg_provider_detail)
+
+        get :index, format: :json
+
+        provider = JSON.parse(response.body, symbolize_names: true).first
+        expect(provider[:totalDischarges]).to eq(drg_provider_detail.total_discharges)
+        expect(provider[:averageCoveredCharges]).to eq(drg_provider_detail.average_covered_charges)
+        expect(provider[:averageMedicarePayments]).to eq(drg_provider_detail.average_medicare_payments)
+        expect(provider[:averageTotalPayments]).to eq(drg_provider_detail.average_total_payments)
+      end
     end
 
-    it 'should render all payments fields by default' do
-      drg_provider_detail = create(:drg_provider_detail)
+    context 'invalid request' do
+      it 'should return bad request status' do
+        get :index, format: :json, params: {page: -1}
 
-      get :index, format: :json
-
-      provider = JSON.parse(response.body, symbolize_names: true).first
-      expect(provider[:totalDischarges]).to eq(drg_provider_detail.total_discharges)
-      expect(provider[:averageCoveredCharges]).to eq(drg_provider_detail.average_covered_charges)
-      expect(provider[:averageMedicarePayments]).to eq(drg_provider_detail.average_medicare_payments)
-      expect(provider[:averageTotalPayments]).to eq(drg_provider_detail.average_total_payments)
+        expect(response.status).to eq(400)
+      end
     end
 
     context 'pagination' do
-      let(:drg_provider_details) { create_list(:drg_provider_detail, 8) }
-      setup do
-        @original_page_size = DrgProviderDetail.per_page
-        DrgProviderDetail.per_page = 5
-        drg_provider_details
-      end
-
-      teardown { DrgProviderDetail.per_page = @original_page_size }
+      let!(:drg_provider_details) { create_list(:drg_provider_detail, 8) }
 
       it 'should fetch the given page number' do
-        get :index, format: :json, params: {page: 2}
+        get :index, format: :json, params: {page: 2, page_size: 5}
 
         actual_provider_details = assigns(:drg_provider_details).to_a
         expect(actual_provider_details.size).to eq(3)
@@ -65,6 +77,14 @@ RSpec.describe DrgProviderDetailsController, type: :controller do
         actual_provider_details = assigns(:drg_provider_details).to_a
         expect(actual_provider_details.size).to eq(2)
         expect(actual_provider_details).to eq(drg_provider_details[4..5])
+      end
+
+      it 'should add pagination metadata in response headers' do
+        get :index, format: :json, params: {page: 3, page_size: 2}
+
+        expect(response.get_header('X-Total-Record-Count')).to eq(8)
+        expect(response.get_header('X-Page-Number')).to eq('3')
+        expect(response.get_header('X-Page-Size')).to eq('2')
       end
 
     end
